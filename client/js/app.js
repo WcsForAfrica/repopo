d3ae5ca8c88312bb4d98be34e5c5447c0db877c9
Christopher Fremond <christopher.fremond@gmail.com>
2018-04-14 13:37:14 +0200

    squelette

---
+++ client/js/app.js
@@ -0,0 +1,6 @@
+import {createHeader} from './components/nav.js'
+/*import {createnavbar} from './components/nav.js'*/
+
+
+const headerEl = document.getElementById('header')
+headerEl.innerHTML = createHeader({})
